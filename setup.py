bee2a2957c6b465512592aeee4ce7b41c74cbe57
ShokofehVS <sh.vahidian@gmail.com>
2022-05-06 10:51:27 +0200

    Add Changes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='biclustlib',
+      version='0.1',
+      description='Library of biclustering algorithms, evaluation measures and dataset',
+      url='https://github.com/ShokofehVS/SeCCA',
+      author='...',
+      author_email='...',
+      license='MIT',
+      packages=['biclustlib'],
+      zip_safe=False)
