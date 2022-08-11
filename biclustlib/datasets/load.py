fac45a358eb92576824a906b82dfd6b1d6b3d0c5
ShokofehVS <sh.vahidian@gmail.com>
2022-08-11 15:15:19 +0200

    Modify python file description

--- biclustlib/datasets/load.py
+++ biclustlib/datasets/load.py
@@ -1,12 +1,3 @@
-"""
-    SeCCA: A Python library of privacy-preserved biclustering algorithm (Cheng and Church) with Homomorphic Encryption
-
-    Copyright (C) 2022  Shokofeh VahidianSadegh
-
-    This file is part of SeCCA.
-
-"""
-
 from os.path import dirname, join
 import numpy as np
 import pandas as pd
