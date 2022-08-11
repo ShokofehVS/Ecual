9e9fbe20b47417a0fb3bedc64ecd2adaa258309d
ShokofehVS <sh.vahidian@gmail.com>
2022-08-11 15:10:03 +0200

    Modify python file description

--- biclustlib/evaluation/subspace.py
+++ biclustlib/evaluation/subspace.py
@@ -1,12 +1,3 @@
-"""
-    SeCCA: A Python library of privacy-preserved biclustering algorithm (Cheng and Church) with Homomorphic Encryption
-
-    Copyright (C) 2022  Shokofeh VahidianSadegh
-
-    This file is part of SeCCA.
-
-"""
-
 import sys
 import numpy as np
 
