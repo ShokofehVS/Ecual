f6a1feff876032307932e369d78c508bddc88f78
ShokofehVS <sh.vahidian@gmail.com>
2022-08-11 15:49:22 +0200

    Modify python file description

--- biclustlib/algorithms/__init__.py
+++ biclustlib/algorithms/__init__.py
@@ -1,15 +1,12 @@
 """
-    SeCCA: A Python library of privacy-preserved biclustering algorithm (Cheng and Church) with Homomorphic Encryption
+    Ecual: A Python library of privacy-preserved biclustering algorithm (Cheng and Church) with Homomorphic Encryption
 
     Copyright (C) 2022  Shokofeh VahidianSadegh
 
-    This file is part of SeCCA.
+    This file is part of Ecual.
 
 """
 
 from .cca import ChengChurchAlgorithm
-from .secca import SecuredChengChurchAlgorithm
-from .type1 import SecuredChengChurchAlgorithmType1
-from .type2 import SecuredChengChurchAlgorithmType2
-from .type3 import SecuredChengChurchAlgorithmType3
-from .type4 import SecuredChengChurchAlgorithmType4
+from .ecual import ecual
+
