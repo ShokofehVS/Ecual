e6640f19e17946be4d9c10b9eafd341b75e00dfb
ShokofehVS <sh.vahidian@gmail.com>
2022-10-23 14:51:00 +0200

    Configuration Setup

--- biclustlib/algorithms/EncryptedMsrCalculator.py
+++ biclustlib/algorithms/EncryptedMsrCalculator.py
@@ -4,7 +4,8 @@ import numpy as np
 
 class EncryptedMsrCalculator():
 
-    @cnp.circuit({"data": "encrypted"})
+    configuration = cnp.Configuration(global_p_error=3 / 100_000, verbose=True)
+    @cnp.circuit({"data": "encrypted"}, configuration)
     def squared_residues(data: cnp.tensor[cnp.uint16, 10, 5]):
         data_mean = np.sum(data) // data.size
         row_means = np.sum(data, axis=1, keepdims=True) // data.shape[1]
@@ -17,17 +18,17 @@ class EncryptedMsrCalculator():
         squared_residues = residues ** 2
         return squared_residues
 
-    @cnp.circuit({"squared_residues": "encrypted"})
+    @cnp.circuit({"squared_residues": "encrypted"}, configuration)
     def msr_calculator(squared_residues: cnp.tensor[cnp.uint16, 10, 5]):
         msr = np.sum(squared_residues) // squared_residues.size
         return msr
 
-    @cnp.circuit({"squared_residues": "encrypted"})
+    @cnp.circuit({"squared_residues": "encrypted"}, configuration)
     def row_msr_calculator(squared_residues: cnp.tensor[cnp.uint16, 10, 5]):
         row_msr = np.sum(squared_residues, axis=1) // squared_residues.shape[1]
         return row_msr
 
-    @cnp.circuit({"squared_residues": "encrypted"})
+    @cnp.circuit({"squared_residues": "encrypted"}, configuration)
     def column_msr_calculator(squared_residues: cnp.tensor[cnp.uint16, 10, 5]):
         col_msr = np.sum(squared_residues, axis=0) // squared_residues.shape[0]
         return col_msr
